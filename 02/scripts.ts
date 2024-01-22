c096679741d52694c5101b3490f69046670a9875
Jamile Moreira <jamilemoreira2310@gmail.com>
2024-01-22 10:38:37 -0300

    exercicio 2

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -1,69 +1,59 @@
 type Item = {
-    nome: string,
-    descricao: string,
-    valor: number
+  nome: string
+  descricao: string
+  valor: number
 }
 
 type Cartao = {
-    numero: number,
-    validade: string,
-    nome: string,
-    cvv: number
+  numero: number
+  validade: string
+  nome: string
+  cvv: number
 }
 
 type Carrinho = {
-    item: Item,
-    qtd: number,
-    desconto: number,
-    frete: number,
-    tipoTransacao: 'credito' | 'Debito',
-    cartao: Cartao
+  item: Item
+  qtd: number
+  desconto: number
+  frete: number
+  tipoTransacao: 'credito' | 'Debito'
+  cartao: Cartao
 }
 
 type Endereco = {
-    cep: string,
-    rua: string,
-    bairro: string,
-    cidade: string,
-    estado: string
+  cep: string
+  rua: string
+  bairro: string
+  cidade: string
+  estado: string
 }
 
 type NovoCarrinho = Omit<Carrinho, 'tipoTransacao'> & {
-    endereco: Endereco,
-    tipoTransacao: Lowercase<'credito' | 'Debito'>
+  endereco: Endereco
+  tipoTransacao: Lowercase<'credito' | 'Debito'>
 }
 
-// type NovoCarrinho = {
-//     item: Item,
-//     qtd: number,
-//     desconto: number,
-//     frete: number,
-//     tipoTransacao: Lowercase<'credito' | 'Debito'>,
-//     cartao: Cartao
-//     endereco: Endereco
-// }
-
 const venda: NovoCarrinho = {
-    item: {
-        nome: 'string',
-        descricao: 'string',
-        valor: 123
-    },
-    qtd: 3,
-    desconto: 12,
-    frete: 100,
-    tipoTransacao: 'debito',
-    cartao: {
-        numero: 123,
-        validade: 'string',
-        nome: 'string',
-        cvv: 123
-    },
-    endereco: {
-        cep: 'string',
-        rua: 'string',
-        bairro: 'string',
-        cidade: 'string',
-        estado: 'string'
-    }
+  item: {
+    nome: 'string',
+    descricao: 'string',
+    valor: 123,
+  },
+  qtd: 3,
+  desconto: 12,
+  frete: 100,
+  tipoTransacao: 'debito',
+  cartao: {
+    numero: 123,
+    validade: 'string',
+    nome: 'string',
+    cvv: 123,
+  },
+  endereco: {
+    cep: 'string',
+    rua: 'string',
+    bairro: 'string',
+    cidade: 'string',
+    estado: 'string',
+  },
 }
