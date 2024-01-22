c957d174dec65b202b929256709c95e084879762
Jamile Moreira <jamilemoreira2310@gmail.com>
2024-01-22 10:48:41 -0300

    exercicio 3

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -1,28 +1,28 @@
 type Conn = {
-    username: string,
-    password: string,
-    host: string,
-    port: string,
-    dbname: string
+  username: string
+  password: string
+  host: string
+  port: string
+  dbname: string
 }
 
 type Conexao = {
-    driver: string,
-    url: string
+  driver: string
+  url: string
 }
 
 const conexao = (dados: Conn): Readonly<Conexao> => {
-    const { username, password, host, port, dbname } = dados;
-    return {
-        driver: 'postgres',
-        url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`
-    };
+  const { username, password, host, port, dbname } = dados
+  return {
+    driver: 'postgres',
+    url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`,
+  }
 }
 
 const conecta = conexao({
-    username: 'guido',
-    password: '123',
-    host: 'localhost',
-    port: '5432',
-    dbname: 'principal'
-});
+  username: 'jamile',
+  password: '123',
+  host: 'localhost',
+  port: '5432',
+  dbname: 'principal',
+})
